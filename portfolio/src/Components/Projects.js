738623c476295d288aae24a00ad23c69a4df89f2
Kiichi <kiichiro.tatsuzawa@gmail.com>
2024-09-05 22:20:41 +1000

    Updated Project Section Animations
    The transitions are now bound dynamically to the section height rather than hard coded.

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -2,34 +2,57 @@ import '../Styles/Style.css';
 import Carousel from './Carousel';
 
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function Projects() {
-  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  const [sectionHeight, setHeight] = useState(0);
+  const [YPos, setYPos] = useState(0);
 
-  useEffect(() => {
-    const handleResize = () => setViewportHeight(window.innerHeight);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  const sectionRef = useRef(null);
   const { scrollY } = useScroll();
 
-  function toPx (val) { return val * viewportHeight / 100; }
+  useEffect(() => {
+    const handleResize = () => {
+      if (sectionRef.current) {
+        const element = sectionRef.current.getBoundingClientRect();
+        setHeight(element.height); 
+        setYPos(element.top);
+      }
+    };
+
+    handleResize();
+
+    const resizeObserver = new ResizeObserver(() => { handleResize(); });
+    const currentRef = sectionRef.current;
+
+    if (currentRef) { resizeObserver.observe(currentRef); }
+
+    return () => {
+      if (currentRef) {
+        resizeObserver.unobserve(currentRef);
+      }
+    };
+  }, []); 
+
+  const start_EaseIn = YPos - window.innerHeight * 0.75;
+  const end_EaseIn = YPos;
+  const start_EaseOut = YPos + sectionHeight - window.innerHeight * 0.75;
+  const end_EaseOut = YPos + sectionHeight;
 
   const opacity = useTransform(
     scrollY,
-    [toPx(50), toPx(90), toPx(480), toPx(520)],
+    [start_EaseIn, end_EaseIn, start_EaseOut, end_EaseOut],
     [0, 1, 1, 0] 
   );
 
   const scale = useTransform(
     scrollY,
-    [toPx(50), toPx(90), toPx(480), toPx(520)],
+    [start_EaseIn, end_EaseIn, start_EaseOut, end_EaseOut],
     ["95%", "100%", "100%", "95%"] 
   );
 
   return (
-    <section id="ProjectSection">
+    <section id="ProjectSection" ref={sectionRef}>
       <motion.div className={'sticky-text'} style={{opacity, scale}}>
           <h1>WEBSITES AND APPS</h1>
           <p>Websites and apps designed and/or developed by me.</p>
