27956cf3df5d83d84ecda7999ef9051778b2b06b
KiichiKatsu <kiichiro.tatsuzawa@gmail.com>
2024-09-08 20:40:54 +1000

    Added flexible Drawer framework for projects
    Added a draggable drawer component for projects.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,149 @@
 import '../Styles/Style.css';
+import React, { useEffect, useState } from 'react';
+import { motion, useDragControls, useMotionValue, useAnimate } from "framer-motion";
+import useMeasure from "react-use-measure";
 
-import GitHubIcon from '../Assets/GitHub.svg';
+function Cards({ data }) {
+    const [activeIndex, setActiveIndex] = useState(null);
+    const [scope, animate] = useAnimate();
 
-function Cards() {
+    const [drawerRef, { height }] = useMeasure();
+
+    const controls = useDragControls();
+    const yDrag = useMotionValue(0);
+
+    const handleCardClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const closeOverlay = async () => {
+        animate(scope.current, {
+            opacity: [1, 0]
+        });
+
+        const yStart = typeof yDrag.get() === "number" ? yDrag.get() : 0;
+        await animate('#drawer', {
+            y: [yStart, height]
+        })
+        setActiveIndex(null);
+    };
+
+    useEffect(() => {
+        if (activeIndex !== null) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [activeIndex]);
+
     return (
-        <div className="Card">
-            <div className="cardDisplay">
-                <img src={GitHubIcon}  alt="My GitHub"/>
-            </div>
-            <h3>Title</h3>
-            <p className="light">Sub-title</p>
-        </div>
+        <>
+            {data.map((item, index) => (
+                <div className="Card" key={index} onClick={() => handleCardClick(index)}>
+                    <div className="cardDisplay">
+                        <img src={item[0]} alt={item[1]} />
+                    </div>
+                    <h3>{item[1]}</h3>
+                    <p className="light">{item[2]}</p>
+
+                    {activeIndex === index && (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 100 }}
+                            className="drawerContainer"
+                            ref={scope}
+                            onClick={(e) => { e.stopPropagation(); closeOverlay(); }}
+                        >
+                            <motion.div
+                                id='drawer'
+                                ref={drawerRef}
+                                onClick={(e) => { e.stopPropagation(); }}
+                                initial={{ y: "100%" }}
+                                animate={{ y: "0%" }}
+                                style={{ y: yDrag }}
+                                transition={{ ease: "easeInOut" }}
+                                onDragEnd={() => { if (yDrag.get() >= 100) { closeOverlay(); } }}
+                                drag="y"
+                                dragControls={controls}
+                                dragListener={false}
+                                dragConstraints={{ top: 0, bottom: 0 }}
+                                dragElastic={{ top: 0, bottom: 0.5 }}
+                            >
+                                <div className='drawerHandle'>
+                                    <button
+                                        onPointerDown={(e) => controls.start(e)}
+                                    />
+                                </div>
+                                <div className='drawerContent'>
+                                    <h1>DRAWER {item[1]}</h1>
+                                    <h2>
+                                        Laudantium corrupti neque rerum labore tempore sapiente. Quos, nobis
+                                        dolores. Esse fuga, cupiditate rerum soluta magni numquam nemo
+                                        aliquid voluptate similique deserunt!
+                                    </h2>
+                                    <h2>
+                                        Rerum inventore provident laboriosam quo facilis nisi voluptatem
+                                        quam maxime pariatur. Velit reiciendis quasi sit magni numquam quos
+                                        itaque ratione, fugit adipisci atque est, tenetur officiis explicabo
+                                        id molestiae aperiam? Expedita quidem inventore magni? Doloremque
+                                        architecto mollitia, dicta, fugit minima velit explicabo sapiente
+                                        beatae fugiat accusamus voluptatum, error voluptatem ab asperiores
+                                        quo modi possimus.
+                                    </h2>
+                                    <h2>
+                                        Laudantium corrupti neque rerum labore tempore sapiente. Quos, nobis
+                                        dolores. Esse fuga, cupiditate rerum soluta magni numquam nemo
+                                        aliquid voluptate similique deserunt!
+                                    </h2>
+                                    <h2>
+                                        Rerum inventore provident laboriosam quo facilis nisi voluptatem
+                                        quam maxime pariatur. Velit reiciendis quasi sit magni numquam quos
+                                        itaque ratione, fugit adipisci atque est, tenetur officiis explicabo
+                                        id molestiae aperiam? Expedita quidem inventore magni? Doloremque
+                                        architecto mollitia, dicta, fugit minima velit explicabo sapiente
+                                        beatae fugiat accusamus voluptatum, error voluptatem ab asperiores
+                                        quo modi possimus.
+                                    </h2>
+                                    <h2>
+                                        Laudantium corrupti neque rerum labore tempore sapiente. Quos, nobis
+                                        dolores. Esse fuga, cupiditate rerum soluta magni numquam nemo
+                                        aliquid voluptate similique deserunt!
+                                    </h2>
+                                    <h2>
+                                        Rerum inventore provident laboriosam quo facilis nisi voluptatem
+                                        quam maxime pariatur. Velit reiciendis quasi sit magni numquam quos
+                                        itaque ratione, fugit adipisci atque est, tenetur officiis explicabo
+                                        id molestiae aperiam? Expedita quidem inventore magni? Doloremque
+                                        architecto mollitia, dicta, fugit minima velit explicabo sapiente
+                                        beatae fugiat accusamus voluptatum, error voluptatem ab asperiores
+                                        quo modi possimus.
+                                    </h2>
+                                    <h2>
+                                        Laudantium corrupti neque rerum labore tempore sapiente. Quos, nobis
+                                        dolores. Esse fuga, cupiditate rerum soluta magni numquam nemo
+                                        aliquid voluptate similique deserunt!
+                                    </h2>
+                                    <h2>
+                                        Rerum inventore provident laboriosam quo facilis nisi voluptatem
+                                        quam maxime pariatur. Velit reiciendis quasi sit magni numquam quos
+                                        itaque ratione, fugit adipisci atque est, tenetur officiis explicabo
+                                        id molestiae aperiam? Expedita quidem inventore magni? Doloremque
+                                        architecto mollitia, dicta, fugit minima velit explicabo sapiente
+                                        beatae fugiat accusamus voluptatum, error voluptatem ab asperiores
+                                        quo modi possimus.
+                                    </h2>
+                                </div>
+                            </motion.div>
+
+                        </motion.div>
+                    )}
+                </div>
+            ))}
+        </>
     )
 }
 
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,18 +1,39 @@
 import '../Styles/Style.css';
-
 import Cards from './Cards.js'
 
+import GitHubIcon from '../Assets/GitHub.svg';
+import LinkedInIcon from '../Assets/LinkedIn.svg';
+import EmailIcon from '../Assets/Mail.svg';
+
 function Carousel() {
+    const Design = [
+        [GitHubIcon, 'Title 1', 'Subtitle 1'],
+        [GitHubIcon, 'Title 2', 'Subtitle 2'],
+        [GitHubIcon, 'Title 3', 'Subtitle 3'],
+    ];
+
+    const Research = [
+        [LinkedInIcon, 'Title 1', 'Subtitle 1'],
+        [LinkedInIcon, 'Title 2', 'Subtitle 2'],
+        [LinkedInIcon, 'Title 3', 'Subtitle 3'],
+    ];
+
+    const Other = [
+        [EmailIcon, 'Title 1', 'Subtitle 1'],
+        [EmailIcon, 'Title 2', 'Subtitle 2'],
+        [EmailIcon, 'Title 3', 'Subtitle 3'],
+    ];
+
     return (
         <div className="Projects">
             <div>
-                <Cards />
+                <Cards data={Design}/>
             </div>
             <div>
-                <Cards />
+                <Cards data={Research}/>
             </div>
             <div>
-                <Cards />
+                <Cards data={Other}/>
             </div>
         </div>
     )
